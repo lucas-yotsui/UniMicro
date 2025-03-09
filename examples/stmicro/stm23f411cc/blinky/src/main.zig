272d9fcf0c318b49a7a74f6ba32f27b4affe59cf
Lucas Yotsui <lucas.yotsui@outlook.com>
2025-03-09 01:57:15 -0300

    Blinky example is now a proper blinky

--- examples/stmicro/stm23f411cc/blinky/src/main.zig
+++ examples/stmicro/stm23f411cc/blinky/src/main.zig
@@ -9,27 +9,18 @@ pub fn main() !void {
 
     gpio.portC.configure_pins(&.{
         .{ .number = 13, .mode = .OUTPUT, .output_speed = .FAST },
-        .{ .number = 14, .mode = .INPUT, .pullup_or_pulldown = .PULL_DOWN },
     });
 
-    gpio.portC.lock_configuration(&.{ 13, 14 }) catch |err| switch (err) {
+    gpio.portC.lock_configuration(&.{13}) catch |err| switch (err) {
         error.LockFailed => @panic("Failed to lock gpio configuration!"),
     };
 
     while (true) {
         // OBS: For some reason, the builtin LED on blackpill
         // boards turns ON on when pin C13 is set to LOW.
-        gpio.portC.set_pin(13, !gpio.portC.read_pin(14));
-        for (1..1_000_000) |_| asm volatile ("");
+        gpio.portC.set_pin(13, true);
+        for (1..5_000_000) |_| asm volatile ("");
+        gpio.portC.set_pin(13, false);
+        for (1..5_000_000) |_| asm volatile ("");
     }
-}
-
-fn example_interrupt() void {
-    while (true) {}
-}
-
-comptime {
-    UniMicro.interrupts.create_nvic(&.{
-        .{ .source = .spi5_global, .handler = example_interrupt },
-    });
 }
